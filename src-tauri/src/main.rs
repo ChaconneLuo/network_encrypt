24765a602934624e2f0dd656c978d673a28bbee4
ChaconneLuo <asgardannila@gmail.com>
2023-06-27 11:18:27 +0800

    feat: init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use rand::Rng;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn generate_random_number_list(length: u64, min: u64, max: u64) -> Vec<u64> {
+    let mut rng = rand::thread_rng();
+    let mut numbers: Vec<u64> = Vec::new();
+    for _ in 0..length {
+        numbers.push(rng.gen_range(min..max));
+    }
+    numbers
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, generate_random_number_list])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [numberList, setNumberList] = useState([]);
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  async function generate_number_list(length: number, min: number, max: number) {
+    setNumberList(await invoke("generate_random_number_list", { length, min, max }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+          generate_number_list(10, 1, 100);
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+      {numberList.length && numberList.map((num: number) => {
+        return <p>{num}</p>;
+      })}
+    </div>
+  );
+}
+
+export default App;
